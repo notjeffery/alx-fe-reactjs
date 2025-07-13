56a9c56e518b60505ffff37616328e969cc56e3b
Jeffery Onyenwe <jconyenwe@gmail.com>
2025-07-13 22:59:56 +0100

    Integrate WelcomeMessage JSX component into App

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,4 @@
+import WelcomeMessage from './components/WelcomeMessage';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -8,6 +9,11 @@ function App() {
 
   return (
     <>
+
+      {/* Hello everyone, I'm learning React at ALX!*/}
+      <WelcomeMessage />
+
+      
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
