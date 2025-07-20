9e2976eedddae86a0f0761107211f91891a17a63
Jeffery Onyenwe <jconyenwe@gmail.com>
2025-07-20 23:06:47 +0100

    Implement Counter component with useState

--- alx-react-app-new/src/App.js
+++ alx-react-app-new/src/App.js
@@ -1,16 +1,11 @@
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import UserProfile from './components/UserProfile';
-import Footer from './components/Footer';
+// src/App.js
+import Counter from './components/Counter';
 
 function App() {
   return (
-    <div>
-      <Header />
-      <MainContent />
-      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-      <Footer />
-    </div>
+    <>
+      <Counter />
+    </>
   );
 }
 
