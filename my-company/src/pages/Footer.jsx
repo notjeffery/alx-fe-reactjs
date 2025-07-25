867f05c6b715297dc2e7b63a06dc3d4e106361a7
Jeffery Onyenwe <jconyenwe@gmail.com>
2025-07-25 07:31:19 +0100

    Complete simple React company website with routing and inline styling

---
+++ my-company/src/pages/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ textAlign: 'center', padding: '10px', backgroundColor: '#eee' }}>
+      &copy; 2025 My Company. All rights reserved.
+    </footer>
+  );
+}
+
+export default Footer;
