03e1c60bd06ef128860f5fb5dbe2ee3ad2f60d7b
Jeffery Onyenwe <jconyenwe@gmail.com>
2025-07-27 18:57:27 +0100

    Fixed App.jsx router

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,31 +1,27 @@
 // src/App.jsx
-import { Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeList from './components/RecipeList';
 import RecipeDetails from './components/RecipeDetails';
 
 function App() {
   return (
-    <div style={{ padding: '1rem' }}>
-      <h1>Recipe Sharing App</h1>
-      <nav>
-        <Link to="/">Home</Link>
-      </nav>
+    <BrowserRouter>
+      <div style={{ padding: '1rem' }}>
+        <h1>Recipe Sharing App</h1>
+        <nav>
+          <Link to="/">Home</Link>
+        </nav>
 
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <AddRecipeForm />
-              <RecipeList />
-            </>
-          }
-        />
-        <Route path="/recipe/:id" element={<RecipeDetails />} />
-      </Routes>
-    </div>
-  );
-}
-
-export default App;
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <AddRecipeForm />
+                <RecipeList />
+              </>
+            }
+          />
+          <Route path="/recipe/:id" element={<RecipeDetails />} />
+        </Rou
