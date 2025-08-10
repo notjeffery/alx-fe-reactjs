ccdf562dfec728e5fef5fcdfa888d06d3422c75f
Jeffery Onyenwe <jconyenwe@gmail.com>
2025-08-10 19:37:29 +0100

    feat: added styled UserProfile component with Tailwind CSS

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,12 +1,11 @@
-export default function App() {
+import UserProfile from "./components/UserProfile";
+
+function App() {
   return (
-    <div className="p-6">
-      <h1 className="text-4xl font-bold underline text-blue-500">
-        Hello Tailwind!
-      </h1>
-      <p className="mt-4 text-lg text-gray-600">
-        If this is styled, Tailwind is working 🎉
-      </p>
+    <div>
+      <UserProfile />
     </div>
-  )
+  );
 }
+
+export default App;
