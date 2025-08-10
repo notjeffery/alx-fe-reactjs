1c34bb4692715fec75b6b18b9c5abf77ef7486c2
Jeffery Onyenwe <jconyenwe@gmail.com>
2025-08-10 19:31:24 +0100

    Feat: added Tailwind CSS integration in React Project

---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,12 @@
+export default function App() {
+  return (
+    <div className="p-6">
+      <h1 className="text-4xl font-bold underline text-blue-500">
+        Hello Tailwind!
+      </h1>
+      <p className="mt-4 text-lg text-gray-600">
+        If this is styled, Tailwind is working 🎉
+      </p>
+    </div>
+  )
+}
