2b75d455fae5d4e6a331433dea3d01e413878d0e
Jeffery Onyenwe <jconyenwe@gmail.com>
2025-08-21 21:10:09 -0400

    Add useAuth hook in ProtectedRoute for protected routing

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,6 +1,19 @@
+import React from "react";
 import { Navigate } from "react-router-dom";
 
-export default function ProtectedRoute({ children }) {
-  const isAuth = localStorage.getItem("auth") === "true";
-  return isAuth ? children : <Navigate to="/login" />;
-}
+const useAuth = () => {
+  const user = { loggedIn: true }; 
+  return user && user.loggedIn;
+};
+
+const ProtectedRoute = ({ children }) => {
+  const isAuth = useAuth();
+
+  if (!isAuth) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
+};
+
+export default ProtectedRoute;
